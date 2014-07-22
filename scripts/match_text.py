cb847c6c5255bdeb0ce73f3ce9525507bb80531d
Bartosz Tarnawski <bartek.tarnawski@gmail.com>
2014-07-22 15:17:03 +0200

    Started with training data text matching

---
+++ scripts/match_text.py
@@ -0,0 +1,93 @@
+import xml.etree.ElementTree as ET
+import itertools
+import unicodedata # Used ?
+import re
+import ast
+import codecs
+import argparse
+
+from collections import defaultdict
+
+NUM_LEN = len('2740170 ')
+
+
+def full_text(node):
+    return ''.join(t for t in node.itertext()).rstrip()
+
+
+def get_dict(root, text_file):
+    str_d = defaultdict(list)
+    for line in text_file:
+        num = int(line.split()[0])
+        aff = line[NUM_LEN:].rstrip()
+        str_d[num] += [aff]
+
+    node_d = defaultdict(list)
+    for aff in root:
+        num = int(aff.get('num'))
+        node_d[num] += [full_text(aff)]
+
+    return str_d, node_d
+
+# Tests if s1 is a subsequence of s2
+def is_subseq(s1, s2):
+    i = 0
+    for l in s1:
+        while s2[i] != l:
+            i += 1
+            if i == len(s2):
+                return False
+    return True
+
+
+def get_matching(l1, l2):
+    mat = [[is_subseq(s1, s2) for s2 in l2] for s1 in l1]
+    success = all(len(filter(lambda x: x, v)) == 1 for v in mat)
+    if not success:
+        return [], False
+    matching = [(s1, filter(lambda x: is_subseq(s1, x), l2)[0]) for s1 in l1]
+    return matching, True
+
+
+def match_text(root, text_file, doc_num):
+    str_dict, node_dict = get_dict(root, text_file)
+    
+    total = 0
+    good = 0
+
+    if doc_num != 0:
+       str_list = str_dict[doc_num]
+       node_list = node_dict[doc_num]
+
+       matching, success = get_matching(node_list, str_list)
+       print matching, success
+    else:
+       for (number, l1) in node_dict.items():
+           matching, success = get_matching(l1, str_dict[number])
+           if success:
+               good += len(l1)
+           total += len(l1)
+       print good / float(total), good, total 
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Wanna gork text into text, huh?")
+    
+    parser.add_argument('--xml', dest='xml', default='resources/set/affs-stripped.xml')
+    parser.add_argument('--raw', dest='raw', default='resources/set/affs-string.txt')
+    parser.add_argument('--doc', dest='doc', type=int, default=0)
+    
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = get_args()
+
+    text_file = codecs.open(args.raw, 'rb', encoding='utf8')
+
+    tree = ET.parse(args.xml)
+    root = tree.getroot()
+    
+    match_text(root, text_file, args.doc)
+
--- test_features.sh
+++ test_features.sh
@@ -6,7 +6,7 @@ affs=resources/final.xml
 model=crf/data/tmpls_chain.txt
 
 python scripts/export2.py --train $train --test $tst --input \
-    $affs --number 200 --neighbor 0 "$1"
+    $affs --number 1000 --neighbor 0 --rare 10 "$1"
 
 java -Xmx2000M \
     -cp $GRMM/class:$GRMM/lib/mallet-deps.jar:$GRMM/lib/grmm-deps.jar \
